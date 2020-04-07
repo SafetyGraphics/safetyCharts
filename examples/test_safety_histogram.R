4cbf4e49b2c139aaed415abf160b70a1a4b34464
LJZeng <zenia.lijuan.zeng@gmail.com>
2020-04-07 00:40:26 -0400

    reorg examples folder and update test_safety_hist

--- examples/test_safety_histogram.R
+++ examples/test_safety_histogram.R
@@ -1,15 +1,14 @@
+library(dplyr)
+library(ggplot2)
 
-config <- list()
-config[["description"]] <- "Test page"
-config[["data"]] <- "https://raw.githubusercontent.com/RhoInc/data-library/master/data/clinical-trials/renderer-specific/adbds.csv"
-config[["settings"]] <- safetyGraphics::generateSettings("sdtm", charts="safetyhistogram")
+source("R/safety_histogram.R")
 
 data <- read.csv('https://raw.githubusercontent.com/RhoInc/data-library/master/data/clinical-trials/renderer-specific/adbds.csv',
                  stringsAsFactors = FALSE, na.strings = c("NA",""))
-settings <- config[["settings"]]
-settings[["unit_col"]] <- "STRESU"
-description <- config[["description"]]
 
-source("R/safety_histogram.R")
-p <- safety_histogram(data=data, settings=settings, description=config$description)
-ggsave("examples/test_safety_histogram.png", plot=p, dpi=300)
+TestSettings <- safetyGraphics::generateSettings(standard="sdtm", charts="safetyhistogram")
+TestSettings[["unit_col"]] <- "STRESU"
+TestSettings[["description"]] <- "Test page"
+
+p <- safety_histogram(data=data, settings=TestSettings)
+ggsave("examples/images/test_safety_histogram.png", plot=p, dpi=300)
