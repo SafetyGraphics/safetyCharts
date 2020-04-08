d716ae2aebe500bd4bbb5f878cbbd903463e871b
LJZeng <zenia.lijuan.zeng@gmail.com>
2020-04-07 23:20:38 -0400

    added roxgen to safety_histogram

--- R/safety_histogram.R
+++ R/safety_histogram.R
@@ -7,7 +7,16 @@
 #' histogram generated in the interactive histogram from safetyGraphics.
 #'
 #' @param data  a data frame containing the lab data
-#' @param settings
+#' @param settings The settings list used to generate a histogram like \code{safetyhistogram()}
+#' \itemize{
+#'  \item{id_col}{Participant ID variable name. Default is \code{"USUBJID"}.}
+#'  \item{value_col}{Value of measure variable name. Default is \code{"STRESN"} for SDTM and \code{"AVAL"} for ADaM.}
+#'  \item{measure_col}{Name of measure variable name. Default is \code{"TEST"} for SDTM and \code{"PARAM} for ADaM.}
+#'  \item{normal_col_low}{Variable name for column containing lower limit of normal values. Default is \code{"STNRLO"} for SDTM and \code{"ANRLO"} for ADaM.}
+#'  \item{normal_col_high}{Variable name for column containing upper limit of normal values. Default is \code{"STNRHI"} for SDTM and \code{"ANRHI"} for ADaM.}
+#'  \item{unit_col}{Unit of measure variable name. Default is \code{"STRESU"} for SDTM and \code{"AVALU" for ADaM.}}
+#'  \item{start_value}{ TO UPDATE} 
+#' }
 #'
 #' @import dplyr
 #' @import ggplot2
@@ -21,10 +30,10 @@
 #' \dontrun{
 #' # Create a histogram on lab parameter with SDTM data
 #' data <- read.csv('https://raw.githubusercontent.com/RhoInc/data-library/master/data/clinical-trials/renderer-specific/adbds.csv', stringsAsFactors = FALSE, na.strings = c("NA",""))
-#' TestSettings <- safetyGraphics::generateSettings(standard="sdtm", charts="safetyhistogram")
-#' TestSettings[["unit_col"]] <- "STRESU"
-#' TestSettings[["description"]] <- "Test page"
-#' safety_histogram(data=data, settings=TestSettings)
+#' settings <- safetyGraphics::generateSettings(standard="sdtm", charts="safetyhistogram")
+#' settings[["unit_col"]] <- "STRESU"
+#' settings[["description"]] <- "Test page"
+#' safety_histogram(data=data, settings=settings)
 #' }
 #'
 #' @seealso \link[safetyGraphics]{}
@@ -40,7 +49,7 @@ safety_histogram <- function(data, settings){
   unit_col <- settings[["unit_col"]]
   #start_value <- settings[["start_value"]]
   #details <- settings[["details"]]
-  missingValues <- settings[["missingValues"]] # "" "NA" "N/A"
+  #missingValues <- settings[["missingValues"]] # "" "NA" "N/A"
 
   measure_selected <- ifelse(!is.null(settings[["start_value"]]),
                              settings[["start_value"]],
--- examples/test_safety_histogram.R
+++ examples/test_safety_histogram.R
@@ -3,12 +3,14 @@ library(ggplot2)
 
 source("R/safety_histogram.R")
 
+#library(safetyCharts)
+
 data <- read.csv('https://raw.githubusercontent.com/RhoInc/data-library/master/data/clinical-trials/renderer-specific/adbds.csv',
                  stringsAsFactors = FALSE, na.strings = c("NA",""))
 
-TestSettings <- safetyGraphics::generateSettings(standard="sdtm", charts="safetyhistogram")
-TestSettings[["unit_col"]] <- "STRESU"
-TestSettings[["description"]] <- "Test page"
+settings <- safetyGraphics::generateSettings(standard="sdtm", charts="safetyhistogram")
+settings[["unit_col"]] <- "STRESU"
+settings[["description"]] <- "Test page"
 
-p <- safety_histogram(data=data, settings=TestSettings)
+p <- safety_histogram(data=data, settings=settings)
 ggsave("examples/images/test_safety_histogram.png", plot=p, dpi=300)
