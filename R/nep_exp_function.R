7066f683449614f3e53f11a29c1399ee4a77c085
Sri Vemuri <vemuri_sri@aws-rspro-pne01.alkermes.com>
2020-05-20 16:27:58 +0000

    cleaned up code, added comments.

--- R/nep_exp_function.R
+++ R/nep_exp_function.R
@@ -21,40 +21,37 @@ library(lubridate)
 
 lb1= nep_explorer_data %>% filter(TEST %in% c("Creatinine", "Cystatin C", "eGFR", "eGFRcys")) %>% 
   mutate(PARCAT=CAT, PARAM=TEST, AVAL = STRESN, AVALU= STRESU,ADT=DT, LBDT=DT, LBDTC = as.character(DT))
-#lb1b = lb1 %>%  mutate(LBDTC_ = (str_replace(LBDTC, "[T]", " "))) %>% 
- # mutate(LBDTC1 = ifelse(nchar(LBDTC_)==16, paste(LBDTC_, ":00", sep = ""), LBDTC_)) %>%
-  #mutate(LBDTC2 = ifelse(nchar(LBDTC1)==13, paste(LBDTC1, ":00:00", sep = ""), LBDTC1)) %>%
-  #mutate(LBDTC3 = ifelse(nchar(LBDTC2)==10, paste(LBDTC2, "00:00:00", sep = ""), LBDTC2) , TRTSDTC = paste(RFSTDTC, "00:00:00", sep=""), TRTSDT = as.Date(RFSTDTC))
 
+### if lab parameters have a date and partial time. LBDTTM will hold the final date time
 
 lb1b = lb1 %>% mutate(LBDTC_ = LBDTC) %>%
   mutate(LBDTC1 = ifelse(nchar(LBDTC_)==16, paste(LBDTC_, ":00", sep = ""), LBDTC_)) %>%
   mutate(LBDTC2 = ifelse(nchar(LBDTC1)==13, paste(LBDTC1, ":00:00", sep = ""), LBDTC1)) %>%
   mutate(LBDTC3 = ifelse(nchar(LBDTC2)==10, paste(LBDTC2, " 00:00:00", sep = ""), LBDTC2) , TRTSDTC = paste(RFSTDTC, "00:00:00", sep=""), TRTSDT = as.Date(RFSTDTC))
   
-#lb2 = lb1b %>%  mutate(LBDTTM = ymd_hms(as.POSIXct(strptime(LBDTC3 , "%Y-%m-%d %H:%M")) ), LBDT = as.Date(LBDTC))
+lb2 = lb1b %>%  mutate(LBDTTM = ymd_hms(as.POSIXct(strftime(LBDTC3 , "%F %X")) ), LBDT = as.Date(LBDTC))
+#lb2 = lb1b %>%  mutate(LBDTTM = ymd_hms(as.POSIXct(strptime(LBDTC3 , "%F %X")) ), LBDT = as.Date(LBDTC))
 
 lb2 = lb1b
 
+#########Create baseline flags
 
+#in our example all dates dont have time. So safe to use just LBDT
 sub_dat_dt =lb2 %>%  filter(LBDT<=TRTSDT & !is.na(TRTSDT) & !is.na(LBDT)  & !is.na(AVAL))
 #sub_dat_dttm =lb2 %>%  filter(LBDT<=TRTSDT & !is.na(TRTSDT) & !is.na(LBDT) & !is.na(LBDTTM)  & !is.na(AVAL) )
 
-subq2 = "Select distinct a.* ,min(LBDT) as MINDT from sub_dat_dt as a 
- group by USUBJID, VISITNUM, PARCAT, PARAM, LBDT having LBDT = min(LBDT) "
-sub_datf = sqldf(subq2)
-
+##select last observation on or before TRTSDT
 q2a = "select a.*, case a.PARAM when 'Creatinine' then 'creat' when 'Cystatin C' then 'cystac' when 'eGFR' then 'eGFR_creat' when 'eGFRcys' then 'eGFR_cystac' 
 end as PARAMCD,  b.BASE,  b.BASEDT from lb2 as a left join (select USUBJID,  max(LBDT) as BASEDT ,
-AVAL as BASE, VISITNUM, PARAM,PARCAT from sub_datf  group by USUBJID,PARCAT,PARAM having MAX(LBDT) = LBDT) as b
+AVAL as BASE, VISITNUM, PARAM,PARCAT from sub_dat_dt  group by USUBJID,PARCAT,PARAM having MAX(LBDT) = LBDT) as b
 on a.USUBJID = b.USUBJID and a.PARAM= b.PARAM and a.PARCAT=b.PARCAT"
 
 
 lb3 = sqldf(q2a)
-
+##create flags
 lb3= lb3 %>% mutate(ABLFL = ifelse(BASEDT==LBDT & BASE == AVAL , "Y", NA )) 
 
-
+###derive CHG,PCHG and FCHG values
 lb4 = lb3 %>% 
   mutate(CHG = ifelse(!is.na(TRTSDT) & !is.na(ADT) & ADT >= TRTSDT & is.na(ABLFL) & !is.na(AVAL) & !is.na(BASE), AVAL-BASE, NA), 
          PCHG = ifelse( !is.na(CHG) &CHG != 0 & !is.na(BASE) &BASE !=0, (CHG/BASE)*100, ifelse( !is.na(CHG) &CHG == 0 & !is.na(BASE), 0, NA)),
@@ -86,8 +83,10 @@ lb5_1234 = left_join(lb5_123,lb5_egfr_cystac, by = c("USUBJID","VISITNUM","PARCA
 
 lb6 = lb5_1234 %>% select(USUBJID,VISITNUM,PARCAT,ADT,AGE,SEX,RACE,ARM,ABLFL, starts_with (c("CREAT", "eGFR" ,"CYSTAC")))
 
+################################################################
 ###create maximal change parameter within a specified window
-
+#####below code is ongoing Please do not review
+###################################################################
 lb7 = lb4 %>% filter(PARAMCD %in% c('creat', 'cystac', 'eGFR_creat' ,'eGFR_cystac')) 
 
 
@@ -95,7 +94,7 @@ lb7 = lb4 %>% filter(PARAMCD %in% c('creat', 'cystac', 'eGFR_creat' ,'eGFR_cysta
 
 #lb8_ = sqldf(q3)
 
-#q3 = "select a.* , nrows from lb7 as a left join lb8_ as b on a.USUBJID=b.USUBJID and a.PARAMCD=b.PARAMCD"
+#q3 = "select a.* , nrows from lb7 as a left join lb8_ as b on a.USUBJID=b.USUBJID and a.PARAMCD=b.PARAMCD" 
 
 #lb8_a = sqldf(q3)  
 
