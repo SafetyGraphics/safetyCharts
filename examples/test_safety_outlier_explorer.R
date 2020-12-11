4d9d25bec03c93273efc491ac7168d81500cdb59
rescalan <raescalan@gmail.com>
2020-12-10 23:11:07 -0500

    Update unit testing

--- examples/test_safety_outlier_explorer.R
+++ examples/test_safety_outlier_explorer.R
@@ -21,14 +21,20 @@ settings <- list(
   unit_col="STRESU"
 )
 
+
+# Example 1 ---------------------------------------------------------------
+
 ## Example 1 - Summary with all measures
 
 p1 <- safetyCharts::safety_outlier_explorer(
   data=data, 
   settings=settings
 )
-ggsave("examples/test_safety_outlier_explorer_all.png", plot=p1, dpi=300)
+# ggsave("examples/test_safety_outlier_explorer_all.png", plot=p1, dpi=300)
 
+p1
+
+# Example 2 ---------------------------------------------------------------
 ## Example 2 - Summary with 3 measures
 
 settings$measure_values <- c("Albumin","Bicarbonate","Calcium")
@@ -37,8 +43,11 @@ p2 <- safetyCharts::safety_outlier_explorer(
   settings=settings
 )
 
-ggsave("examples/test_safety_outlier_explorer_subset.png", plot=p2, dpi=300)
+p2
+# ggsave("examples/test_safety_outlier_explorer_subset.png", plot=p2, dpi=300)
 
+
+# Example 3 ---------------------------------------------------------------
 
 ## Example 3 - Summary with 1 measure
 
@@ -49,4 +58,5 @@ p3 <- safetyCharts::safety_outlier_explorer(
   settings=settings
 )
 
-ggsave("examples/test_safety_outlier_explorer_solo.png", plot=p3, dpi=300)
+p3
+# ggsave("examples/test_safety_outlier_explorer_solo.png", plot=p3, dpi=300)
