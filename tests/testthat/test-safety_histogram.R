be7e31734c67eb5843587b518a67cbc91bd5b314
Antonio EC <antonioec@gattaca-machine.lan>
2021-02-04 23:28:05 -0600

    Update to formal unit testing of safety histogram

--- tests/testthat/test-safety_histogram.R
+++ tests/testthat/test-safety_histogram.R
@@ -1,10 +1,25 @@
 
 # test histogram is made correctly ----------------------------------------
 
-test_that("Histogram plot", {
+test_that("Safety histogram is created properly", {
   
-  safetyData::sdtm_lb
-  # expect_equal(2 * 2, 4)
   
+  histogram_settings <- list(
+    id_col="USUBJID", # DONE
+    value_col="LBORRES", # DONE
+    measure_col="LBTEST", # DONE
+    visit_col="VISIT", # DONE
+    studyday_col="LBDY", # DONE
+    normal_col_low="LBORNRLO", # DONE
+    normal_col_high="LBORNRHI", # DONE
+    visitn_col="VISITNUM", # DONE
+    unit_col="LBORRESU", # DONE,
+    measure_values = "Phosphate"
+    # measure_values = "Albumin" 
+  )
+  
+  output_histogram <- safety_histogram(data = safetyData::sdtm_lb, settings = histogram_settings)
+  
+  expect_is(output_histogram, "ggplot")
   
 })
