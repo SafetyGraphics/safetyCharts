2226e350b68824fce9a265d055285a96a59ee006
Antonio EC <antonioec@gattaca-machine.lan>
2021-02-04 23:04:49 -0600

    Update histogram testing

--- examples/test_safety_histogram.R
+++ examples/test_safety_histogram.R
@@ -13,3 +13,44 @@ description <- config[["description"]]
 source("R/safety_histogram.R")
 p <- safety_histogram(data=data, settings=settings, description=config$description)
 ggsave("examples/test_safety_histogram.png", plot=p, dpi=300)
+
+
+
+# test create safety histogram  ---------------------------------------------------------------
+
+
+histogram_settings <- list(
+  id_col="USUBJID", # DONE
+  value_col="LBORRES", # DONE
+  measure_col="LBTEST", # DONE
+  visit_col="VISIT", # DONE
+  studyday_col="LBDY", # DONE
+  normal_col_low="LBSTNRLO", # DONE
+  normal_col_high="LBSTNRHI", # DONE
+  visitn_col="VISITNUM", # DONE
+  unit_col="LBORRESU" # DONE
+)
+
+safety_histogram(data = safetyData::sdtm_lb, settings = histogram_settings)
+
+
+
+# measure values ----------------------------------------------------------
+
+unique(safetyData::sdtm_lb$LBTEST)
+
+histogram_settings <- list(
+  id_col="USUBJID", # DONE
+  value_col="LBORRES", # DONE
+  measure_col="LBTEST", # DONE
+  visit_col="VISIT", # DONE
+  studyday_col="LBDY", # DONE
+  normal_col_low="LBSTNRLO", # DONE
+  normal_col_high="LBSTNRHI", # DONE
+  visitn_col="VISITNUM", # DONE
+  unit_col="LBORRESU", # DONE,
+  # measure_values = "Microcytes",
+  measure_values = "Albumin" 
+)
+
+safety_histogram(data = safetyData::sdtm_lb, settings = histogram_settings)
