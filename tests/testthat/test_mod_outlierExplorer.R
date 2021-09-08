5e698ca0992b859e48ffb5d7fba16e1b375b633c
Jeremy Wildfire <jwildfire@gmail.com>
2021-09-08 09:50:53 -0400

    bump to v0.2

---
+++ tests/testthat/test_mod_outlierExplorer.R
@@ -0,0 +1,33 @@
+context("Tests for the outlierExplorer R module")
+library(safetyCharts)
+library(shiny)
+library(dplyr)
+library(shinytest)
+library(testthat)
+
+if(interactive()){
+    app <- ShinyDriver$new("module_examples/outlierExplorer")
+    initial <- app$getAllValues()
+}
+
+test_that("Chart has image and has correct mapping", {
+    skip_if_not(interactive())
+    expect_equal(substring(initial$output$`example1`$src, 1, 14), "data:image/png")
+    mapping <- initial$output$`example1`$coordmap$panels[[1]]$mapping
+    expect_equal(length(initial$output$`example1`$coordmap$panels), 3)
+    expect_equal(mapping$group, "USUBJID")
+    expect_equal(mapping$x, "VISITDY")
+    expect_equal(mapping$y, "LBORRES")
+    expect_equal(mapping$panelvar1, "LBTEST")
+})
+
+test_that("Changing the tests updates the chart", {
+    skip_if_not(interactive())
+    app$setValue("example1-measures", c("Albumin", "Bilirubin"))
+    Sys.sleep(3) # TODO inplement app$waitForValue() instead of sleeping
+    new <- app$getAllValues()
+    expect_equal(length(new$output$`example1`$coordmap$panels), 2)
+    expectUpdate(app, `example1-measures` = c("Albumin"), "example1-outlierExplorer")
+})
+
+if(interactive()) app$stop()
---
+++ tests/testthat/test_safety_outlier_explorer.R
@@ -0,0 +1,25 @@
+context("safety_outlier_explorer")
+library(safetyData)
+
+# Compare to basic plot ---------------------------------------------------
+
+test_that("Summary with all measures", {
+    settings <- list(
+        id_col = "USUBJID",
+        value_col = "LBSTRESN",
+        measure_col = "LBTEST",
+        visit_col = "VISIT",
+        studyday_col = "LBDY",
+        normal_col_low = "LBSTNRLO",
+        normal_col_high = "LBSTNRHI",
+        visitn_col = "VISITN",
+        unit_col = "LBSTRESU"
+    )
+
+    plot_summary_ALT <- safetyCharts::safety_outlier_explorer(
+        data = safetyData::sdtm_lb %>% filter(LBTEST=="Alanine Aminotransferase"),
+        settings = settings
+    )
+
+    vdiffr::expect_doppelganger("safety_outlier_explorer", plot_summary_ALT)
+})
