3bbd202104ac7f7c25eb0e2bd9b96be7065ac197
Jeremy Wildfire <jwildfire@gmail.com>
2021-11-04 16:56:25 -0400

    add metadata

---
+++ R/meta.R
@@ -0,0 +1,81 @@
+#' Metadata data frame containing information about the data mapping used to configure safetyGraphics charts for the ecg domain. One record per unique data mapping
+#'
+#' @format A data frame with 22 rows and 10 columns
+#' \describe{
+#'    \item{domain}{Data domain}
+#'    \item{text_key}{Text key indicating the setting name. \code{'--'} delimiter indicates a field level data mapping}
+#'    \item{col_key}{Key for the column mapping}
+#'    \item{field_key}{Key for the field mapping (if any)}
+#'    \item{type}{type of mapping - "field" or "column"}
+#'    \item{label}{Label}
+#'    \item{description}{Description}
+#'    \item{multiple}{Mapping supports multiple columns/fields }
+#'    \item{standard_adam}{Default values for the ADaM data standard}
+#'    \item{standard_sdtm}{Default values for the SDTM data standard}
+#' }    
+#' 
+#' @source Created for this package
+
+"meta_ecg"
+
+#' Metadata data frame containing information about the data mapping used to configure safetyGraphics charts for the labs domain. One record per unique data mapping
+#'
+#' @format A data frame with X rows and 10 columns
+#' \describe{
+#'    \item{domain}{Data domain}
+#'    \item{text_key}{Text key indicating the setting name. \code{'--'} delimiter indicates a field level data mapping}
+#'    \item{col_key}{Key for the column mapping}
+#'    \item{field_key}{Key for the field mapping (if any)}
+#'    \item{type}{type of mapping - "field" or "column"}
+#'    \item{label}{Label}
+#'    \item{description}{Description}
+#'    \item{multiple}{Mapping supports multiple columns/fields }
+#'    \item{standard_adam}{Default values for the ADaM data standard}
+#'    \item{standard_sdtm}{Default values for the SDTM data standard}
+#' }    
+#' 
+#' @source Created for this package
+
+"meta_labs"
+
+
+#' Metadata data frame containing information about the data mapping used to configure safetyGraphics charts for the dm domain. One record per unique data mapping
+#'
+#' @format A data frame with X rows and 10 columns
+#' \describe{
+#'    \item{domain}{Data domain}
+#'    \item{text_key}{Text key indicating the setting name. \code{'--'} delimiter indicates a field level data mapping}
+#'    \item{col_key}{Key for the column mapping}
+#'    \item{field_key}{Key for the field mapping (if any)}
+#'    \item{type}{type of mapping - "field" or "column"}
+#'    \item{label}{Label}
+#'    \item{description}{Description}
+#'    \item{multiple}{Mapping supports multiple columns/fields }
+#'    \item{standard_adam}{Default values for the ADaM data standard}
+#'    \item{standard_sdtm}{Default values for the SDTM data standard}
+#' }    
+#' 
+#' @source Created for this package
+
+"meta_dm"
+
+
+#' Metadata data frame containing information about the data mapping used to configure safetyGraphics charts for the ae domain. One record per unique data mapping
+#'
+#' @format A data frame with X rows and 10 columns
+#' \describe{
+#'    \item{domain}{Data domain}
+#'    \item{text_key}{Text key indicating the setting name. \code{'--'} delimiter indicates a field level data mapping}
+#'    \item{col_key}{Key for the column mapping}
+#'    \item{field_key}{Key for the field mapping (if any)}
+#'    \item{type}{type of mapping - "field" or "column"}
+#'    \item{label}{Label}
+#'    \item{description}{Description}
+#'    \item{multiple}{Mapping supports multiple columns/fields }
+#'    \item{standard_adam}{Default values for the ADaM data standard}
+#'    \item{standard_sdtm}{Default values for the SDTM data standard}
+#' }    
+#' 
+#' @source Created for this package
+
+"meta_aes"
--- data-raw/makeMeta.R
+++ data-raw/makeMeta.R
@@ -3,5 +3,18 @@ library(tidyverse)
 library(usethis)
 
 #Copy metadata to /data
+#ecg
 meta_ecg<-read_csv("data-raw/meta_ecg.csv")
 usethis::use_data(meta_ecg, overwrite = TRUE)
+
+#labs
+meta_labs<-read_csv("data-raw/meta_labs.csv")
+usethis::use_data(meta_labs, overwrite = TRUE)
+
+#aes
+meta_aes<-read_csv("data-raw/meta_aes.csv")
+usethis::use_data(meta_aes, overwrite = TRUE)
+
+#dm
+meta_dm<-read_csv("data-raw/meta_dm.csv")
+usethis::use_data(meta_dm, overwrite = TRUE)
