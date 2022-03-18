aec6ba91b81a544020221cc85f88bf17f038b15b
Jeremy Wildfire <jwildfire@gmail.com>
2022-03-18 10:32:49 -0400

    update hepExplorer docs

--- R/hepExplorer.R
+++ R/hepExplorer.R
@@ -2,9 +2,9 @@
 #' 
 #' Convience mapping of render_widget for hepExplorer. 
 #' 
-#' @details The [data](https://github.com/SafetyGraphics/hep-explorer/wiki/Data-Guidelines) and [mapping](https://github.com/SafetyGraphics/hep-explorer/wiki/Configuration) should match the specs described in the [hepExplorer](https://github.com/SafetyGraphics/hep-explorer/wiki/Configuration) javascript library. Items passed in ... are added to mapping, and then the list is converted to json via `jsonlite::toJSON(mapping, auto_unbox=TRUE, null="null").
+#' @details The [data](https://github.com/SafetyGraphics/hep-explorer/wiki/Data-Guidelines) and [mapping](https://github.com/SafetyGraphics/hep-explorer/wiki/Configuration) should match the specs described in the [hepExplorer](https://github.com/SafetyGraphics/hep-explorer/wiki/Configuration) javascript library. Items passed in ... are added to mapping, and then the list is converted to json via `jsonlite::toJSON(mapping, auto_unbox=TRUE, null="null")`.
 #' 
-#' The default mapping - designed to work with the CDSISC AdAM format - like safetyata::adam_adlbc - is:   
+#' The default mapping shown below is designed to work with data in the CDISC ADaM format (like `safetydata::adam_adlbc`).    
 #' 
 #' ```
 #' mapping <- list(
@@ -27,13 +27,13 @@
 #'
 #' Parameters that are not included in the default mapping can be accessed via ...; Key options and defaults for safetyData::adam_adlbc shown below: 
 #'
-#' - `filters`: list of columns to be included as data filters (e.g. filters=c("SEX","AGEGR1"))
-#' - `group_cols`: list of columns used to define grouping and set point color (e.g. filters=c("SEX","AGEGR1"))
+#' - `filters`: list of columns to be included as data filters (e.g. `filters=c("SEX","AGEGR1")``)
+#' - `group_cols`: list of columns used to define grouping and set point color (e.g. `filters=c("SEX","AGEGR1")``)
 #' - `x_options` and `y_options` - specify which labs can be used for x and y axis dropdowns. By default, all options are included on x-axis, but only Bilirubin is shown on y-axis. To allow an interactive y-axis, use `y_options="all"`. 
 #' - `baseline` - flag defining the baseline visit for each participant. `baseline` must be provided to enable the mDish view on the hep-explorer chart. Define as a list with `value_col` and `values` (e.g. `baseline=list(value_col="ABLFL",values="Y")`)
-#' - `title` and `warningText` - Strings used to define the header text shown above the filters. 
+#' - `title` and `warningText` - Strings used to define the header text shown above the filters.  
 #' 
-#' For more options see the [full specs](https://github.com/SafetyGraphics/hep-explorer/wiki/Configuration) for the javascript library.
+#' For more options see the [full specs](https://github.com/SafetyGraphics/hep-explorer/wiki/Configuration) in the javascript library.
 #' 
 #' @examples 
 #' 
@@ -65,7 +65,7 @@
 #'     visitn_col = "VISITNUM"
 #' )
 #' 
-#' @param df data frame containing lab data used to render for hepExplorer. Default is safetyDAta::adam_adlbc. 
+#' @param df data frame containing lab data used to render for hepExplorer. Default is safetyData::adam_adlbc. 
 #' @param mapping named list with the current data mappings. See details for default mapping. 
 #' @param ... additional options to be added to mapping. Will overwrite mapping. 
 #' 
