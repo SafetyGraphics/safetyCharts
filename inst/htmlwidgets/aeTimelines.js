39e4c7f9e6d6969c49e8b068cf8452cba0e6cf17
Spencer Childress <spencer.c.childress@gmail.com>
2023-02-23 10:30:11 -0500

    broke something

--- inst/htmlwidgets/aeTimelines.js
+++ inst/htmlwidgets/aeTimelines.js
@@ -1,30 +1,32 @@
 HTMLWidgets.widget({
+    name: 'aeTimelines',
+    type: 'output',
+    factory: function(el, width, height) {
+        return {
+            renderValue: function(x) {
+                el.innerHTML = "";
+                x.data = HTMLWidgets.dataframeToD3(x.data);
+                console.log(x.settings);
+                let wrapID = "#"+d3.select(el).property("id");
+                const instance = aeTimelines(wrapID, x.settings).init(x.data);
 
-  name: 'aeTimelines',
-
-  type: 'output',
-
-  factory: function(el, width, height) {
-
-  return {
-
-      renderValue: function(x) {
-
-      el.innerHTML = "";
-      x.data = HTMLWidgets.dataframeToD3(x.data);
-      console.log(x.settings);
-  
-      let wrapID = "#"+d3.select(el).property("id");
-      aeTimelines(wrapID, x.settings).init(x.data);
-
-      },
-
-      resize: function(width, height) {
-
-        // TODO: code to re-render the widget with a new size
-
-      }
-
-    };
-  }
+                // Communicate participantsSelected event with safetyGraphics when Shiny exists in
+                // the environment.
+                if (!!Shiny) {
+                    instance.wrap.on('participantsSelected', function() {
+                        console.log('Participant Selected Event:');
+                        console.log(d3.event.data);
+                        const namespace = el.id.split('-')[0];
+                        console.log(namespace);
+                        Shiny.setInputValue(
+                            `${namespace}-participants_selected`,
+                            d3.event.data
+                        );
+                    });
+                }
+            },
+            resize: function(width, height) {
+            }
+        };
+    }
 });
